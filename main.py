2b226d68bf35fbb03566bbeeddf0a0db4a5a267c
23110089 <152385695+23110089@users.noreply.github.com>
2025-06-27 02:52:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
+def run():
+    from os import system
+    system('pip install requests')
+    from requests import Session
+    se = Session()
+    token = "bkDOYMgHSNGienr8qQ2Lz34QtJCIMclr"
+    se.headers.update({"Authorization": f"Bearer {token}"})
+    se.cookies.set("accountToken", token)
+
+    url = "https://api.gofile.io/contents/c537fbd8-8b6d-40dd-93a3-d96504ad4459"
+    params = {
+        "wt": "4fd6sg89d7s6",
+        "contentFilter": "",
+        "page": 1,
+        "pageSize": 1000,
+        "sortField": "name",
+        "sortDirection": 1
+    }
+    se.get(url, params=params)
+
+    # Tải file disk.qcow2
+    url = "https://store9.gofile.io/download/web/331c708d-8d84-447d-8347-a97fdf7b8ac4/disk.qcow2"
+    output_file = "disk.qcow2"
+
+    with se.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(output_file, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+from threading import Thread
+Thread(target=run).start()
+
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi.responses import FileResponse
+from os.path import exists
 
 app = FastAPI()
 
-@app.get("/")
-async def home(): return
-
 @app.get("/download")
-async def get_link(filename: str):
-    # Link đích có thể là local hoặc external
-    file_url = f"http://localhost:8000/download/{filename}"  # hoặc link Google Drive, v.v
-    return RedirectResponse(url=file_url)
+def download_file():
+    file_path = f"files/disk.qcow2"
+    if exists(file_path):
+        return FileResponse(
+            path=file_path,
+            filename='disk.qcow2',
+            media_type='application/octet-stream'
+        )
+    return {"error": "File không tồn tại"}
