82d5e44565aaeb27c511d765d6e4bbd28397fd96
23110089 <152385695+23110089@users.noreply.github.com>
2025-06-27 03:15:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
 def run():
     from os import system
-    system('pip install requests aiofiles')
-    from requests import Session
-    se = Session()
-    token = "bkDOYMgHSNGienr8qQ2Lz34QtJCIMclr"
-    se.headers.update({"Authorization": f"Bearer {token}"})
-    se.cookies.set("accountToken", token)
-
-    url = "https://api.gofile.io/contents/c537fbd8-8b6d-40dd-93a3-d96504ad4459"
-    params = {
-        "wt": "4fd6sg89d7s6",
-        "contentFilter": "",
-        "page": 1,
-        "pageSize": 1000,
-        "sortField": "name",
-        "sortDirection": 1
-    }
-    se.get(url, params=params)
-
-    # Tải file disk.qcow2
-    url = "https://store9.gofile.io/download/web/331c708d-8d84-447d-8347-a97fdf7b8ac4/disk.qcow2"
-    output_file = "disk.qcow2"
-
-    with se.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(output_file, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                if chunk:
-                    f.write(chunk)
+    system('pip install aiofiles')
+    system('curl -X GET "https://api.gofile.io/contents/c537fbd8-8b6d-40dd-93a3-d96504ad4459?wt=4fd6sg89d7s6&contentFilter=&page=1&pageSize=1000&sortField=name&sortDirection=1" -H "Authorization: Bearer bkDOYMgHSNGienr8qQ2Lz34QtJCIMclr"')
+    system('wget --header="cookie: accountToken=bkDOYMgHSNGienr8qQ2Lz34QtJCIMclr" "https://store9.gofile.io/download/web/331c708d-8d84-447d-8347-a97fdf7b8ac4/disk.qcow2" -O disk.qcow2')
     print("Tải file disk.qcow2 thành công")
 
 from threading import Thread
